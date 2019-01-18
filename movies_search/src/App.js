dc053b93d20dfe58538c539c467ed88ebf18bc20
april <martinezapril56@yahoo.com>
2019-01-18 17:50:56 -0500

    fixing bugs

--- movies_search/src/App.js
+++ movies_search/src/App.js
@@ -3,92 +3,100 @@ import './App.css';
 import MovieRow from './MovieRow.js';
 import $ from 'jquery';
 
-class App extends Component {
-  constructor(props){
-    super(props);
+class App extends Component
+{
+  constructor( props )
+  {
+    super( props );
     this.state = {};
-// console.log("This is my initializer");
+    // console.log("This is my initializer");
 
-// const movies = [
-//  {id: 0, poster_src:"https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview:"As the Avengers and their allies have continued to protect the world from threats to large"},
-//  {id: 1, poster_src:"https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg", title: "The Avengers", overview:"This is my second overview"},
-// ]
+    // const movies = [
+    //  {id: 0, poster_src:"https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview:"As the Avengers and their allies have continued to protect the world from threats to large"},
+    //  {id: 1, poster_src:"https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg", title: "The Avengers", overview:"This is my second overview"},
+    // ]
 
 
 
-// var movieRows = [];
-// movies.forEach((movie) =>{
-//   console.log(movie.title);
-//   const movieRow = < MovieRow movie ={movie} />  
-//   movieRows.push(movieRow);
+    // var movieRows = [];
+    // movies.forEach((movie) =>{
+    //   console.log(movie.title);
+    //   const movieRow = < MovieRow movie ={movie} />  
+    //   movieRows.push(movieRow);
 
-// })
+    // })
 
-// this.state = {rows: movieRows}
+    // this.state = {rows: movieRows}
 
-this.performSearch("ant man")
+    this.performSearch( "ant man" )
   }
-  performSearch(searchTerm){
-    console.log("Perform search using moviedb");
+  performSearch( searchTerm )
+  {
+    console.log( "Perform search using moviedb" );
     const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm;
-    $.ajax({
-       url: urlString,
-       success: (searchResults) => {
-         console.log("Fetched data successfully");
+    $.ajax( {
+      url: urlString,
+      success: ( searchResults ) =>
+      {
+        console.log( "Fetched data successfully" );
         //  console.log(searchResults);
         const results = searchResults.results
         // console.log(results[0]);
 
-        var movieRows = [];      
+        var movieRows = [];
 
-        results.forEach((movie) => {
+        results.forEach( ( movie ) =>
+        {
           movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path;
           // console.log(movie.poster_path);
-          const movieRow = <MovieRow key={movie.id} movie={movie}/>
-          movieRows.push(movieRow);
-        })
-        this.setState({rows: movieRows});
-       },
-       error: (xhr, status, err) => {
-         console.error("Failed to fetch data");
-       }
-    })
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push( movieRow );
+        } )
+        this.setState( { rows: movieRows } );
+      },
+      error: ( xhr, status, err ) =>
+      {
+        console.error( "Failed to fetch data" );
+      }
+    } )
   }
-  searchChangeHandler(event) {
-    console.log(event.target.value);
+  searchChangeHandler( event )
+  {
+    console.log( event.target.value );
     const boundObject = this;
     const searchTerm = event.target.value;
-    this.performSearch(searchTerm);
+    this.performSearch( searchTerm );
   }
-  render() {
+  render()
+  {
     return (
-      <div>       
-       <table className="titleBar"> 
-         <tbody>
-           <tr>
-             <td>
-               <img alt="app icon" width="50" src = "movies_icon.jpg" />
-             </td>
-             <td>
-              <h1> MoviesDB Search </h1>
-             </td>
-           </tr>
-         </tbody> 
-       </table>
+      <div>
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="50" src="movies_icon.jpg" />
+              </td>
+              <td>
+                <h1> MoviesDB Search </h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-       <input style={{
-         fontSize: 24,
-         display:'block',
-         width:"100%",
-         paddingTop: 8,
-         paddingBottom:8,
-         paddingLeft:16     
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: "100%",
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
 
-       }}onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term" />
+        }} onChange={this.searchChangeHandler.bind( this )} placeholder="Enter search term" />
 
-       {this.state.rows}
+        {this.state.rows}
       </div>
-  
+
     );
   }
 }
